d9ba15630f41352049c5c442ac90e0156095e15a
Dhruv Anand <ananddhruv29@gmail.com>
2025-09-17 19:26:05 +0530

    Add HTML, CSS & JS files

---
+++ script.js
@@ -0,0 +1,6 @@
+function showDate() {
+    document.getElementById("date").innerText =
+        "Today's date is: " + new Date().toLocaleDateString();
+}
+
+// TODO: Add a function that changes background color when called
